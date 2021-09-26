745621549c8928b1169cf348bb891814def8cbba
Khhs167 <34920681+Khhs167@users.noreply.github.com>
2021-09-26 17:10:30 +0200

    Added everything afaik

---
+++ BasicBoy/Numerics.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicBoy
+{
+
+    public class vshort : CustomValueType<vshort, SByte>
+    {
+        private vshort(SByte value) : base(value) { }
+        public static implicit operator vshort(SByte value) { return new vshort(value); }
+        public static implicit operator SByte(vshort custom) { return custom._value; }
+    }
+
+    public class uvshort : CustomValueType<uvshort, byte>
+    {
+        private uvshort(byte value) : base(value) { }
+        public static implicit operator uvshort(byte value) { return new uvshort(value); }
+        public static implicit operator byte(uvshort custom) { return custom._value; }
+    }
+
+    public class CustomValueType<TCustom, TValue>
+    {
+        protected readonly TValue _value;
+
+        public CustomValueType(TValue value)
+        {
+            _value = value;
+        }
+
+        public override string ToString()
+        {
+            return _value.ToString();
+        }
+
+        public static bool operator <(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
+        {
+            return Comparer<TValue>.Default.Compare(a._value, b._value) < 0;
+        }
+
+        public static bool operator >(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
+        {
+            return !(a < b);
+        }
+
+        public static bool operator <=(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
+        {
+            return (a < b) || (a == b);
+        }
+
+        public static bool operator >=(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
+        {
+            return (a > b) || (a == b);
+        }
+
+        public static bool operator ==(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
+        {
+            return a.Equals((object)b);
+        }
+
+        public static bool operator !=(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
+        {
+            return !(a == b);
+        }
+
+        public static TCustom operator +(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
+        {
+            return (dynamic)a._value + b._value;
+        }
+
+        public static TCustom operator -(CustomValueType<TCustom, TValue> a, CustomValueType<TCustom, TValue> b)
+        {
+            return ((dynamic)a._value - b._value);
+        }
+
+        protected bool Equals(CustomValueType<TCustom, TValue> other)
+        {
+            return EqualityComparer<TValue>.Default.Equals(_value, other._value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((CustomValueType<TCustom, TValue>)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return EqualityComparer<TValue>.Default.GetHashCode(_value);
+        }
+    }
+}
--- BasicBoy/Program.cs
+++ BasicBoy/Program.cs
@@ -1,12 +1,198 @@
 ﻿using System;
+using System.IO;
 using Raylib_cs;
 namespace BasicBoy
 {
-    class BasicBoy
+    public static class BasicBoy
     {
+        public static ushort codePtr;
+
+        public static byte[] rom = new byte[0];
+
+        public static byte[] ram = new byte[64 * 1024];
+        public static byte[] vram = new byte[32 * 1024];
+        public static byte[] displayBuffer = new byte[128 * 128 * 2];
+        public static byte flags;
+
+        public static string err = String.Empty;
+
+
+        public static bool GetFlag(byte flag)
+        {
+            return (flags & (1 << flag)) != 0;
+        }
+
         static void Main(string[] args)
         {
-            
+            Raylib.InitWindow(128, 128, "BasicBoy");
+
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.BLACK);
+            Raylib.DrawText("Loading", 5, 5, 10, Color.WHITE);
+            Raylib.EndDrawing();
+            var gameLocation = Directory.GetCurrentDirectory() + "/test.bbg";
+            rom = File.ReadAllBytes(gameLocation);
+
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.BLACK);
+            Raylib.EndDrawing();
+
+            flags = 0b00000001;
+            displayBuffer[0] = 255;
+
+
+            while (codePtr < rom.Length && !Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.BLACK);
+                for (int x = 0; x < 128; x++)
+                {
+                    for (int y = 0; y < 128; y++)
+                    {
+                        int i = (x + (GetFlag(0) ? 128 : 0)) * 128 + y;
+
+                        Raylib.DrawPixel(x, y, new Color(displayBuffer[i], displayBuffer[i], displayBuffer[i], (byte)255));
+                    }
+                }
+
+                Raylib.DrawText(err, 0, 0, 10, Color.RED);
+
+                Raylib.EndDrawing();
+
+                if (err != string.Empty)
+                    continue;
+
+                byte opcode = rom[codePtr];
+
+                if(opcode == 0x00)
+                {
+                    codePtr++;
+                    continue;
+                }
+                if(opcode == 0x01)
+                {
+                    codePtr++;
+                    ushort ramLoc = GetValue(codePtr);
+                    codePtr += 3;
+                    ram[ramLoc] = (byte)GetValue(codePtr);
+                    codePtr += 3;
+                    continue;
+                }
+                if (opcode == 0x02)
+                {
+                    codePtr++;
+                    ushort ramLoc = GetValue(codePtr);
+                    codePtr += 3;
+                    ram[ramLoc] = (byte)(ram[ramLoc] & (byte)GetValue(codePtr));
+                    codePtr += 3;
+                    continue;
+                }
+                if (opcode == 0x03)
+                {
+                    codePtr++;
+                    ushort ramLoc = GetValue(codePtr);
+                    codePtr += 3;
+                    ram[ramLoc] = (byte)(ram[ramLoc] | (byte)GetValue(codePtr));
+                    codePtr += 3;
+                    continue;
+                }
+                if (opcode == 0x04)
+                {
+                    codePtr++;
+                    ushort ramLoc = GetValue(codePtr);
+                    codePtr += 3;
+                    ram[ramLoc] = (byte)(ram[ramLoc] !& (byte)GetValue(codePtr));
+                    codePtr += 3;
+                    continue;
+                }
+                if (opcode == 0x05)
+                {
+                    codePtr++;
+                    ushort ramLoc = GetValue(codePtr);
+                    codePtr += 3;
+                    ram[ramLoc] = (byte)(ram[ramLoc] ^ (byte)GetValue(codePtr));
+                    codePtr += 3;
+                    continue;
+                }
+                if (opcode == 0x06)
+                {
+                    codePtr++;
+                    ushort ramLoc = GetValue(codePtr);
+                    codePtr += 3;
+                    ram[ramLoc] = (byte)(ram[ramLoc] !| (byte)GetValue(codePtr));
+                    codePtr += 3;
+                    continue;
+                }
+
+                if (opcode == 0x07)
+                {
+                    codePtr++;
+                    ushort ramLoc = GetValue(codePtr);
+                    codePtr += 3;
+                    ram[ramLoc] = (byte)(ram[ramLoc] + (byte)GetValue(codePtr));
+                    codePtr += 3;
+                    continue;
+                }
+                if (opcode == 0x08)
+                {
+                    codePtr++;
+                    ushort ramLoc = GetValue(codePtr);
+                    codePtr += 3;
+                    ram[ramLoc] = (byte)(ram[ramLoc] - (byte)GetValue(codePtr));
+                    codePtr += 3;
+                    continue;
+                }
+                if (opcode == 0x09)
+                {
+                    codePtr++;
+                    ushort ramLoc = GetValue(codePtr);
+                    codePtr += 3;
+                    ram[ramLoc] = (byte)(ram[ramLoc] / (byte)GetValue(codePtr));
+                    codePtr += 3;
+                    continue;
+                }
+                if (opcode == 0x0A)
+                {
+                    codePtr++;
+                    ushort ramLoc = GetValue(codePtr);
+                    codePtr += 3;
+                    ram[ramLoc] = (byte)(ram[ramLoc] * (byte)GetValue(codePtr));
+                    codePtr += 3;
+                    continue;
+                }
+                if (opcode == 0x0B)
+                {
+                    codePtr++;
+                    ushort ramLoc = GetValue(codePtr);
+                    codePtr += 3;
+                    bool change = ram[ramLoc] == (byte)GetValue(codePtr);
+                    codePtr += 3;
+                    if (change)
+                        codePtr = GetValue(codePtr);
+                    codePtr += 3;
+                    continue;
+                }
+
+                if (opcode == 0x0C)
+                {
+                    codePtr = GetValue((ushort)(codePtr + 1));
+                    continue;
+                }
+
+                err = "Invalid opcode: 0x" + Convert.ToHexString(new byte[] { opcode});
+
+                
+            }
+        }
+
+        public static ushort GetValue(ushort loc)
+        {
+            if (rom[loc] == 0x00)
+                return BitConverter.ToUInt16(new byte[] { rom[loc + 1], rom[loc + 2] });
+            else if (rom[loc] == 0x01)
+                return ram[loc];
+
+            return 0;
         }
     }
 }
